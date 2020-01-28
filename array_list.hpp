b911d7e9e1d180c0d913ee9ea3bcd1202e9a3799
Myles Thiessen <myles.thiessen@mail.utoronto.ca>
2020-01-28 15:57:48 -0500

    arraylist fixed intentation

--- array_list.hpp
+++ array_list.hpp
@@ -31,24 +31,24 @@ private:
   /* Default Size, this is the default size of the primitive array */
   const std::size_t default_size = 50;
 
-	/* Default load factor. This represents the percentage of the default array that needs to be filled in order for there to be a copy function called */
+  /* Default load factor. This represents the percentage of the default array that needs to be filled in order for there to be a copy function called */
   const float default_load_factor = 1.0f;
 
-	/* Load factor, either default_load_factor or user defined*/
+  /* Load factor, either default_load_factor or user defined*/
   float load_factor;
 
-	/* Primitive array size */
+  /* Primitive array size */
   std::size_t array_size;
 
-	/* Size of user inputs */
+  /* Size of user inputs */
   std::size_t dynamic_size;
 
-	/* Primitive array */
+  /* Primitive array */
   T *data;
 
-	/*
-	 * Doubles the primitive array size and copies all the data from the original array to the new array. Then resets the data pointer to the new array
-	 */
+  /*
+   * Doubles the primitive array size and copies all the data from the original array to the new array. Then resets the data pointer to the new array
+   */
   void copy() {
     this->array_size *= 2;
 
@@ -61,64 +61,64 @@ private:
     this->data = new_data;
   }
 
-	/*
-	 *
-	 * @param position
-	 * @return true if valid index and false if invalid
-	 */
-	bool index_check(const std::size_t position){
-		return position > 0 && position < this->dynamic_size;
-	}
+  /*
+   *
+   * @param position
+   * @return true if valid index and false if invalid
+   */
+  bool index_check(const std::size_t position) {
+    return position > 0 && position < this->dynamic_size;
+  }
 
 public:
 
-	/* */
+  /* */
   array_list(const std::size_t default_capacity, const float load_factor) : array_size(default_capacity), load_factor(load_factor), data(new T[array_size]()) {}
 
-	/* */
+  /* */
   array_list(const std::size_t default_capacity) : array_list(default_capacity, default_load_factor) {}
 
-	/* */
+  /* */
   array_list() : array_list(default_size, default_load_factor) {}
 
-	/*
-	 */
+  /*
+   */
   const T& get(const std::size_t position) {
     if(!this->index_check()) throw "Out of bounds";
 
     return this->data[position];
   }
 
-	/*
-	 */
+  /*
+   */
   void push(const T& data) {
     if(this->dynamic_size >= this->load_factor * this->array_size) this->copy();
 
     this->data[this->dynamic_size++] = data;
   }
 
-	/*
-	 */
+  /*
+   */
   void set(const std::size_t position, const T& data) {
     if(!this->index_check()) throw "Out of bounds";
 
     this->data[position] = data;
   }
 
-	/*
-	 */
+  /*
+   */
   std::size_t size() {
     return this->dynamic_size;
   }
 
-	/*
-	 */
+  /*
+   */
   int size_int() {
     return this->dynamic_size;
   }
 
-	/*
-	 */
+  /*
+   */
   ~array_list() {
     delete[] this->data;
   }
